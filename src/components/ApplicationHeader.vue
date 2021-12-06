50b40d10dc54a22b20659b871ece527cb2b1dd9a
B3hniya <B3hniya@yahoo.com>
2021-12-06 14:24:07 +0330

    Application Header component added to the project.

---
+++ src/components/ApplicationHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-toolbar-title>
+        Quasar App
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+<script>
+export default {
+  name: 'application-header'
+}
+</script>
