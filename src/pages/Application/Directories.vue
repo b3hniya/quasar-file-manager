bcc75ddcb35722346a0c453f83e5024bcd32f407
B3hniya <B3hniya@yahoo.com>
2021-12-06 14:30:19 +0330

    2 Pages added to the project.
    *Directories page : is a page that works under Application Layout, and main functionality of application is being done by this part.
    
    *Entry page: is a page responsible for users login and logout.

---
+++ src/pages/Application/Directories.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <h1>THIS IS DIRS</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
---
+++ src/pages/Entry/Registration.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+
+    <h1>this is registration page</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
