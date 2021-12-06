637ffbdc92eef3a4fbb7503da023ba5688e5b00d
B3hniya <B3hniya@yahoo.com>
2021-12-06 14:26:39 +0330

    2 Layouts added to the Project
    * Application Layout responsible for what we do in the main part of application and its route represented as '/app'.
    
    * Entry Layout responsible for user entrance to the application via signing up or signing in, and it's route represented as '/entry'.

---
+++ src/layouts/Application.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout>
+    <application-header/>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import ApplicationHeader from "components/ApplicationHeader";
+
+export default {
+  name: 'ApplicationLayout',
+  components: {ApplicationHeader},
+}
+</script>
