55c700e60f07c3d80a5eb3cfdfa5c651a1ad8563
B3hniya <B3hniya@yahoo.com>
2021-12-07 09:07:04 +0330

    RegisterComp and its test added to project.
    RegisterComp.vue is the component responsible for user entrance to the application and, make api calls and inputs possible in application.

---
+++ src/components/RegisterComp.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <q-input
+      outlined
+      color="dark"
+      v-model="email"
+      type="text"
+      data-test="email-field"
+      class="q-mt-lg q-mb-md entry-fields"
+      label="Email"
+    />
+    <q-input
+      outlined
+      color="dark"
+      v-model="password"
+      type="password"
+      data-test="password-field"
+      class="q-mt-md entry-fields"
+      label="Password"
+    />
+    <q-btn
+      @click="submit"
+      data-test="submit-btn"
+      class="q-px-xl q-my-md"
+      color="dark"
+      text-color="blue-1"
+    >
+      Register / Login
+    </q-btn>
+  </div>
+</template>
+
+<script>
+import entryBoot from "../boot/entryBoot";
+import { Cookies } from "quasar";
+
+export default {
+  name: "RegisterComp",
+  data: () => ({
+    email: "",
+    password: "",
+  }),
+  methods: {
+    async submit() {
+      const res = entryBoot()
+        .post("/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .catch((e) => {
+          //TODO => it should show an error
+          console.log(e.response.data.message);
+        });
+
+      if (res.status === 200) {
+        Cookies.set("access_token", res.data.data.access_token, {
+          expires: "1d",
+        });
+        Cookies.set("refresh_token", res.data.data.refresh_token, {
+          expires: "1d",
+        });
+        Cookies.set("root_dir_id", res.data.data.root_directory_id, {
+          expires: "1d",
+        });
+        this.$store.commit(
+          "directories/mutateRootDirID",
+          res.data.data.root_directory_id
+        );
+        this.$router.push("/app");
+      }
+
+      this.email = "";
+      this.password = "";
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.entry-fields
+  border-radius: 8px
+  background-color: #fff
+</style>
---
+++ test/jest/__tests__/Registration.spec.js
@@ -0,0 +1,91 @@
+import { mount, createLocalVue } from "@vue/test-utils";
+import Registration from "../../../src/components/RegisterComp";
+import * as All from "quasar";
+import Vuex from "vuex";
+
+const { Quasar } = All;
+
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const components = Object.keys(All).reduce((object, key) => {
+  const val = All[key];
+  if (val && val.component && val.component.name != null) {
+    object[key] = val;
+  }
+  return object;
+}, {});
+
+describe("Mount Registration page", () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+  localVue.use(Quasar, { components }); // , lang: langEn
+
+  const wrapper = mount(Registration, {
+    localVue,
+  });
+  const vm = wrapper.vm;
+
+  let emailField = wrapper.find("[data-test='email-field']");
+  let passField = wrapper.find("[data-test='password-field']");
+  let submitBtn = wrapper.find("[data-test='submit-btn']");
+
+  async function fillEmailField(val) {
+    emailField.setValue(val);
+  }
+
+  async function fillPasswordField(val) {
+    passField.setValue(val);
+  }
+
+  describe("emailField Related Tests", () => {
+    it("has email field.", () => {
+      expect(emailField.element.value).toBe("");
+    });
+
+    it("email value will update after input.", async () => {
+      await fillEmailField("test@test.com");
+      expect(vm.email).toBe("test@test.com");
+    });
+  });
+
+  describe("password related tests.", () => {
+    it("has password field", () => {
+      expect(passField.element.value).toBe("");
+    });
+
+    it("password value will update after input.", async () => {
+      await fillPasswordField("testPassword");
+      expect(vm.password).toBe("testPassword");
+    });
+  });
+
+  describe("submit button related tests", () => {
+    it("has a button", () => {
+      expect(submitBtn.element.innerHTML).toContain("Register/Login");
+    });
+
+    it("after trigerring the button the submit btn password field will get empty", async () => {
+      await fillEmailField("test@test1.com");
+      await fillPasswordField("testPass1");
+      await submitBtn.trigger("click");
+      await timeout(300);
+      expect(vm.password).toBe("");
+    });
+
+    it("after triggering the button email field should be empty", async () => {
+      await fillPasswordField("testPass1");
+      await fillEmailField("test@test1.com");
+      await submitBtn.trigger("click");
+      await timeout(300);
+      expect(vm.email).toBe("");
+    });
+
+    // it("call the api and brings you the answere.", async () => {
+    //   await fillPasswordField("testPass1");
+    //   await fillEmailField("test@test1.com");
+    //   await timeout(300);
+    // })
+  });
+});
