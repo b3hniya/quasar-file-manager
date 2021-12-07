2f4c1ed8067efde87aebd5d1ba1fc188ddb79fed
B3hniya <B3hniya@yahoo.com>
2021-12-07 11:55:27 +0330

    Empty directory component added to the project.

--- src/pages/Application/Directories.vue
+++ src/pages/Application/Directories.vue
@@ -1,11 +1,22 @@
 <template>
-  <q-page padding style="background-color: #f1f1f1">
-    <h1>THIS IS DIRS</h1>
+  <q-page padding style="background-color: #f1f1f1" class="flex flex-center">
+    <!--  TODO => directory path should be shown in the application  -->
+
+    <!--  TODO => if root dir was empty then show empty folder component.   -->
+
+    <empty-directory />
+
+    <!--  TODO => else show all roots in the application.  -->
+    <!--    <h1>THIS IS DIRS</h1>-->
   </q-page>
 </template>
 
 <script>
+import EmptyDirectory from "components/EmptyDirectory";
+
 export default {
+  components: { EmptyDirectory },
+
   // name: 'PageName',
-}
+};
 </script>
