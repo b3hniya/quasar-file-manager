dc6a87a2a8afdea268f3db0a8f31f16792482c0d
B3hniya <B3hniya@yahoo.com>
2021-12-07 09:07:59 +0330

    page title and RegisterComp added to the project.

--- src/pages/Entry/Registration.vue
+++ src/pages/Entry/Registration.vue
@@ -1,12 +1,21 @@
 <template>
-  <q-page padding>
-
-    <h1>this is registration page</h1>
+  <q-page padding class="flex flex-center column" id="entry-page">
+    <p id="entry-page-title"> Try this product out for free. </p>
+    <register-comp />
   </q-page>
 </template>
 
 <script>
+import RegisterComp from "components/RegisterComp";
 export default {
-  // name: 'PageName',
+  components: {RegisterComp}
 }
 </script>
+
+<style lang="sass" scoped>
+#entry-page
+  background-color: #f1f1f1
+
+#entry-page-title
+  font-weight: 500
+</style>
