3734170f0bea17d13e104f3867fa8d1c2dd77ed4
B3hniya <B3hniya@yahoo.com>
2021-12-07 16:09:44 +0330

    EmptyDirectory modified and is ready to use.

--- src/components/EmptyDirectory.vue
+++ src/components/EmptyDirectory.vue
@@ -1,14 +1,32 @@
 <template>
-  <div style="display: flex; justify-content: center; align-items: center; flex-direction: column">
-    <q-icon name="inventory" size="128px"> </q-icon>
-    <div class="text-h6 q-mt-md"> There is nothing to show. </div>
-    <q-btn flat class="bg-dark text-white text-weight-light q-px-xl q-py-xs q-mt-lg" > Create Directory </q-btn>
+  <div
+    style="
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    "
+  >
+    <q-icon name="inventory" size="128px"></q-icon>
+    <div class="text-h6 q-mt-md">There is nothing to show.</div>
+    <q-btn
+      flat
+      class="bg-dark text-white text-weight-light q-px-xl q-py-xs q-mt-lg"
+      @click="openCreateDirectoryPopup"
+    >
+      Create Directory
+    </q-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: "EmptyDirectory",
+  methods: {
+    openCreateDirectoryPopup() {
+      this.$store.commit("createDirectory/promptMutation", true);
+    },
+  },
 };
 </script>
 
