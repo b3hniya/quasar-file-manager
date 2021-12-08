72a34c936655f247b889c2343d1321eea13de2e3
B3hniya <B3hniya@yahoo.com>
2021-12-08 17:42:04 +0330

    ApiCaller for essential tasks like refreshing the access token and so on.

---
+++ src/core/entryApiCaller.js
@@ -0,0 +1,20 @@
+import entryBoot from "boot/entryBoot";
+import { Cookies } from "quasar";
+import {
+  setAccessTokenCookie,
+  setRefreshTokenCookie,
+} from "src/core/cookieSetter";
+
+export async function getNewAccessToken() {
+  entryBoot()
+    .post("/refresh", {
+      refresh_token: Cookies.get("refresh_token"),
+    })
+    .then((res) => {
+      setRefreshTokenCookie(res.data.data.refresh_token);
+      setAccessTokenCookie(res.data.data.access_token);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
