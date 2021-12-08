05650399bdab55e134bc8f96f19504a9ba92bbdf
B3hniya <B3hniya@yahoo.com>
2021-12-08 17:39:42 +0330

    related and primal logic added to the directories folder.

--- src/pages/Application/Directories.vue
+++ src/pages/Application/Directories.vue
@@ -1,26 +1,67 @@
 <template>
-  <q-page padding style="background-color: #f1f1f1" class="flex flex-center">
-    <!--  TODO => directory path should be shown in the application  -->
+  <q-page padding style="background-color: #f1f1f1" :class="qPageClass">
+    <template v-if="dir.isEmpty()">
+      <empty-directory />
+    </template>
 
-    <!--  TODO => if root dir was empty then show empty folder component.   -->
+    <template v-else>
+      <directory-button
+        v-for="(dir, index) in dir.subDirs"
+        :key="index"
+        :name="dir.title"
+      />
+    </template>
 
-    <empty-directory />
     <create-directory />
-
-    <!--  TODO => else show all roots in the application.  -->
-    <!--    <h1>THIS IS DIRS</h1>-->
   </q-page>
 </template>
 
 <script>
-import EmptyDirectory from "components/EmptyDirectory";
-import CreateDirectory from "components/CreateDirectory";
+import { Cookies } from "quasar";
+import appBoot from "boot/appBoot";
+import EmptyDirectory from "components/Application/Directory/EmptyDirectory";
+import CreateDirectory from "components/Application/Directory/CreateDirectoryPopUp";
+import DirectoryButton from "components/Application/Directory/DirectoryButton";
+import { getNewAccessToken } from "src/core/entryApiCaller";
 
 export default {
-  components: { CreateDirectory, EmptyDirectory },
+  components: { DirectoryButton, CreateDirectory, EmptyDirectory },
   data: () => ({
+    dir: {
+      subDirs: [""],
+      isEmpty() {
+        return this.subDirs.length === 0;
+      },
+    },
+
     prompt: false,
   }),
-  // name: 'PageName',
+  methods: {
+    getSubDirs(dirId) {
+      appBoot()
+        .get(dirId)
+        .then((res) => {
+          this.dir.subDirs = res.data.data.directories;
+        })
+        .catch((e) => {
+          if (e.response.status === 401)
+            getNewAccessToken().then(() => {
+              this.getSubDirs(dirId);
+            });
+          else console.log(e);
+        });
+    },
+  },
+
+  mounted() {
+    this.getSubDirs(Cookies.get("root_dir_id"));
+  },
+
+  computed: {
+    qPageClass() {
+      if (this.dir.isEmpty()) return "flex flex-center";
+      else return "";
+    },
+  },
 };
 </script>
