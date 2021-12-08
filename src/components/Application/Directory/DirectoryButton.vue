eb91b628c87740f4b23076f1166b129150e42f38
B3hniya <B3hniya@yahoo.com>
2021-12-08 17:43:19 +0330

    DirectoryButton.vue added to the project.
    * its main task is to change current dir and triggers new subdir fetches.

---
+++ src/components/Application/Directory/DirectoryButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    //TODO => change current dir and triggers new subdir fetches.
+    <q-icon name="folder"></q-icon>
+    <span> {{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DirectoryButton",
+  props: ["name"],
+};
+</script>
+
+<style scoped></style>
