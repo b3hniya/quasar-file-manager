17ad9a147d1e0c6e25d7ba74ee2d1b202846fc55
B3hniya <B3hniya@yahoo.com>
2021-12-09 22:12:41 +0330

    EmptyDir.vue refactored during readability.

--- src/components/Application/Directory/EmptyDir.vue
+++ src/components/Application/Directory/EmptyDir.vue
@@ -1,18 +1,11 @@
 <template>
-  <div
-    style="
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-    "
-  >
-    <q-icon name="inventory" size="128px"></q-icon>
+  <div id="empty-dir-container">
+    <q-icon name="inventory" size="128px" />
     <div class="text-h6 q-mt-md">There is nothing to show.</div>
     <q-btn
       flat
-      class="bg-dark text-white text-weight-light q-px-xl q-py-xs q-mt-lg"
       @click="openCreateDirectoryPopup"
+      class="bg-dark text-white text-weight-light q-px-xl q-py-xs q-mt-lg"
     >
       Create Directory
     </q-btn>
@@ -30,4 +23,10 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="sass">
+#empty-dir-container
+  display: flex
+  justify-content: center
+  align-items: center
+  flex-direction: column
+</style>
