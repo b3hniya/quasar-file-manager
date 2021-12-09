4b14dd84a1c9e2b4ef64737c5f4a57d519bbc3ea
B3hniya <B3hniya@yahoo.com>
2021-12-09 14:06:36 +0330

    DirectoryButton shaped and can be use in the project.
    *The remaining part is the more button and what we click and what should happen after our clicking.

--- src/components/Application/Directory/DirectoryButton.vue
+++ src/components/Application/Directory/DirectoryButton.vue
@@ -1,22 +1,50 @@
 <template>
-  <div @click="changeDir">
-    <q-icon name="folder"></q-icon>
-    <span> {{ name }}</span>
+  <div>
+    <q-icon
+      name="more_horiz"
+      size="24px"
+      id="more-icon"
+    />
+
+    <div class="flex items-center column" id="folder-btn">
+      <q-icon @click="changeDir" name="folder" size="96px" />
+      <span> {{ name }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "DirectoryButton",
-  props: ["name", 'id'],
+  props: ["name", "id"],
   methods: {
-    changeDir(){
+    changeDir() {
       this.$store.commit("directories/mutateCurrentDirID", this.id);
       this.$store.commit("directories/mutateCurrentDirName", this.name);
-      this.$store.dispatch("directories/getSubDir")
-    }
-  }
+      this.$store.dispatch("directories/getSubDir");
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="sass" scoped>
+#more-icon
+  border-radius: 50%
+  background-color: rgba(0, 0, 0, 0)
+  transition: background-color 0.2s ease 0s
+  position: absolute
+  margin-left: 64px
+  z-index: 1
+  &:hover
+    background-color: rgba(0, 0, 0, 0.1)
+
+
+#folder-btn
+  max-height: 128px
+  border-radius: 8px
+  background-color: rgba(0, 0, 0, 0)
+  transition: background-color 0.2s ease 0s
+
+  &:hover
+    background-color: rgba(0, 0, 0, 0.1)
+</style>
